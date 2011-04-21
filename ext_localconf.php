a13cc2ac8588458f3a485402ea0b4993bdf518e0
mbirchler@snowflake.ch <mbirchler@snowflake.ch@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-21 08:29:00 +0000

    Task 466.649: Added support for sitemap items (Section index (pagecontent w/Index checked)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/multicolumn/branches/multicolumn_2-1@46835 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,7 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 	// define multicolumn path
 define('PATH_tx_multicolumn', t3lib_extMgm::extPath('multicolumn'));
 define('PATH_tx_multicolumn_rel', t3lib_extMgm::extRelPath($_EXTKEY));
+define('PATH_tx_multicolumn_pi_base', PATH_tx_multicolumn . 'lib/class.tx_multicolumn_pi_base.php');
 
 	// is TYPO3 Version < TYPO3 4-3
 if (t3lib_div::int_from_ver(TYPO3_branch) < t3lib_div::int_from_ver('4.4')) {
@@ -27,7 +28,10 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['tce']['formevals']['tx_multicolumn_tce_eval'] = 
 t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:multicolumn/tsconfig.txt">');
 	//add default TypoScript
 t3lib_extMgm::addTypoScript('multicolumn', 'setup', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:multicolumn/pi1/static/defaultTS.txt">', 43);
+	//add sitemap TypoScript
+t3lib_extMgm::addTypoScript('multicolumn', 'setup', '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:multicolumn/pi_sitemap/static/setup.txt">', 43);
 
 	// add frontend plugin
-t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_multicolumn_pi1.php','_pi1','list_type',1);
+t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_multicolumn_pi1.php','_pi1','list_type', 1);
+t3lib_extMgm::addPItoST43($_EXTKEY,'pi_sitemap/class.tx_multicolumn_pi_sitemap.php','_pi_sitemap','list_type', 1);
 ?>
