808dc56dd5f85d7d70874b24d0b2edc54d4d36f7
mbirchler@snowflake.ch <mbirchler@snowflake.ch@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-23 07:08:11 +0000

    Added hash for tabs
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/multicolumn/branches/multicolumn_2-1@52402 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/effects/simpleTabs/simpleTabs.js
+++ res/effects/simpleTabs/simpleTabs.js
@@ -1,39 +1,44 @@
 jQuery.noConflict();
-jQuery(document).ready(function($){
+jQuery(document).ready(function($){	
 	var effectBox = {
 		options : {}
 		,$el : []
+		,id : 0
 		,$tabItems : []
 		,maxHeight : []
 		,$navigationContainer : []
 		,start : function () {
 			var self = this;
 			$('div.simpleTabs').each(function(index, element){
-				var id = element.id.split('_')[1];
+				self.id = element.id.split('_')[1];
 				
-				self.options  = window['mullticolumnEffectBox_'+id] ? window['mullticolumnEffectBox_'+id] : {};
+				self.options  = window['mullticolumnEffectBox_' + self.id] ? window['mullticolumnEffectBox_' + self.id] : {};
 				self.$el = $(this);
 				self.$simpleTabsContainer = self.$el.find('.simpleTabsContainer'),
 				self.$tabItems = self.$el.find('li.tabItem');
+
+				if(self.$el.length) {
+					self.buildNavigation(index);
+					if(self.options['fixHeight']) self.setContainerHeight();
+				}
 			});
-			
-			if(self.$el.length) {
-				this.buildNavigation();
-				if(self.options['fixHeight']) this.setContainerHeight();
-			}
 		}
 		
-		,buildNavigation : function () {
+		,buildNavigation : function (tabIndex) {
 			var	$navigationAppend = this.$el.find('.simpleTabNavigationContainer'),
 				self = this;
-			
+
+			tabIndex = tabIndex + 1;
 			self.$navigationContainer = $('<ul class="simpleTabNavigation clearfix"></ul>');
 			
 			this.$tabItems.each(function(index){
 				var $el = $(this),
+					$container = $el.parent('ul'),
 					$navigationItemContent = $el.find('.simpleTabNavigationItemContent'),
-					$navigationLabel = $navigationItemContent.text(),
-					$a = $('<a href="#">' + $navigationLabel + '</a>'),
+					navigationLabel = $navigationItemContent.text(),
+					navigationItemId = 'tab-' + tabIndex + '-' + (index + 1),
+					
+					$a = $('<a id="' + navigationItemId + '" href="#' + navigationItemId + '">' + navigationLabel + '</a>'),
 					$item = $('<li class="simpleTabNavigationItem simpleTabNavigationItem' + index + '"></li>');
 				
 				$item.append($a);
@@ -41,11 +46,21 @@ jQuery(document).ready(function($){
 				$navigationItemContent.remove();
 				self.maxHeight.push($el.height());
 				
-				$a.click(function(event){
-					event.preventDefault();
+				var show = function () {
 					self.hideAll();
 					$el.show();
-					$item.addClass('tabItemAct');
+					$item.addClass('tabItemAct');					
+				};
+				
+				$a.click(function(){
+					show()
+				});
+
+					// add target links
+				$container.find('a[href="#' + navigationItemId + '"]').each(function(){
+					$(this).click(function(){
+						show()
+					});
 				});
 				
 				if(index) {
@@ -81,4 +96,12 @@ jQuery(document).ready(function($){
 	};
 	
 	effectBox.start();
+
+		// hash bang
+	if(window.location.hash) {
+		var $href = $(window.location.hash);
+		if($href) {
+			$href.trigger('click');
+		}
+	}
 });
